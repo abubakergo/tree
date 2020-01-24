d5f540969993567972bb0e36c655c7319826e801
abubakergo <abubakergo@gmail.com>
2020-01-24 21:52:42 +0200

    deploy

---
+++ precache-manifest.57c7e625353faca5603a8ad6376f91ed.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "641bf83d4cdd650a790d",
+    "url": "/tree/css/app.140adeb0.css"
+  },
+  {
+    "revision": "26e9cf469595378d17a4",
+    "url": "/tree/css/chunk-vendors.609d2b83.css"
+  },
+  {
+    "revision": "149dfbfc125a966fd8e0a3e5770421fc",
+    "url": "/tree/index.html"
+  },
+  {
+    "revision": "d572c08ca79f8f8270a9",
+    "url": "/tree/js/about.132227c3.js"
+  },
+  {
+    "revision": "641bf83d4cdd650a790d",
+    "url": "/tree/js/app.f2d28ae3.js"
+  },
+  {
+    "revision": "26e9cf469595378d17a4",
+    "url": "/tree/js/chunk-vendors.f3dbd353.js"
+  },
+  {
+    "revision": "211162a7d0268179b132facb305548cd",
+    "url": "/tree/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/tree/robots.txt"
+  }
+]);
